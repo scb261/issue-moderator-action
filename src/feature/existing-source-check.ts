29d503574e4ad65477b71c9607e2b67927028ebd
scb261 <65343233+scb261@users.noreply.github.com>
2026-03-29 19:31:46 +0300

    Check all found URLs for an existing source

--- src/feature/existing-source-check.ts
+++ src/feature/existing-source-check.ts
@@ -70,12 +70,21 @@ export async function checkForExistingSource() {
     return;
   }
 
-  const requestUrl = cleanUrl(issueUrls[0]);
-  const existingExtension = repository.find((extension) =>
-    extension.sources.some((source) =>
-      urlsFromString(source.baseUrl).includes(requestUrl),
-    ),
-  );
+  let existingExtension = null;
+  let requestUrl = "";
+  for (let url of issueUrls) {
+    existingExtension = repository.find((extension) =>
+      extension.sources.some((source) =>
+        urlsFromString(source.baseUrl).includes(cleanUrl(url)),
+      ),
+    );
+
+    if (existingExtension) {
+      requestUrl = url;
+      break;
+    }
+  }
+
   if (!existingExtension) {
     core.info(`Existing extension with the URL "${requestUrl}" was not found.`);
     return;
